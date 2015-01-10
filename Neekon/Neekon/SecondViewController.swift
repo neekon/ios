67443c1b13888982eafa117217d63c13a0b0d186
Eiman Zolfaghari <eiman@uber.com>
2015-01-09 21:18:32 -0800

    Template App

---
+++ Neekon/Neekon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Neekon
+//
+//  Created by Eiman Zolfaghari on 1/9/15.
+//  Copyright (c) 2015 Iranican Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
