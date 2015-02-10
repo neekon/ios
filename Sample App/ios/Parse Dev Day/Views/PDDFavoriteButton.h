2a867399ae378982d79539ac0de10c93efdeea71
Mohsen Azimi <msn.azi@gmail.com>
2015-02-10 15:47:57 -0800

    Remove Parse's sample app

--- Sample App/ios/Parse Dev Day/Views/PDDFavoriteButton.h
+++
@@ -1,9 +0,0 @@
-//
-//  PDDFavoriteButton.h
-//  Parse Dev Day
-//
-//  Copyright (c) 2013 Parse. All rights reserved.
-//
-
-@interface PDDFavoriteButton : UIButton
-@end
--- Sample App/ios/Parse Dev Day/Views/PDDScheduleView.h
+++
@@ -1,9 +0,0 @@
-//
-//  PDDScheduleView.h
-//  Parse Dev Day
-//
-//  Copyright (c) 2013 Parse. All rights reserved.
-//
-
-@interface PDDScheduleView : UITableView
-@end
