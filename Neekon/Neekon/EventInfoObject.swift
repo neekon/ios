8cdf9c57a676c1197de998e99fc2ce408647e588
Eiman Zolfaghari <eiman@uber.com>
2015-03-10 19:36:07 -0700

    Updated EventInfo Object

---
+++ Neekon/Neekon/EventInfoObject.swift
@@ -0,0 +1,46 @@
+//
+//  ScheduleObject.swift
+//  Neekon
+//
+//  Created by Eiman Zolfaghari on 1/11/15.
+//  Copyright (c) 2015 Iranican Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class EventInfoObject : PFObject, PFSubclassing {
+    @NSManaged var eventDate : NSDate?
+    @NSManaged var locationName: String?
+    @NSManaged var locationCity: String?
+    @NSManaged var latitude: String?
+    @NSManaged var longitude: String?
+    
+    override class func load() {
+        superclass()?.load()
+        self.registerSubclass()
+    }
+
+    class func parseClassName() -> String! {
+        return "EventInfo"
+    }
+
+    class func fetchEventInfoObject(resultsBlock: (EventInfoObject!, NSError!) -> Void) {
+        let query = EventInfoObject.query()
+        query.findObjectsInBackgroundWithBlock { (results:[AnyObject]!, error:NSError?) -> Void in
+            if (error != nil) {
+                // TODO: show error
+                resultsBlock(nil, error)
+            } else {
+                if let objects = results {
+                    var newsObjects = [EventInfoObject]()
+                    for newsObject in objects as [EventInfoObject] {
+                        resultsBlock(newsObject, nil)
+                        return
+                    }
+                }
+            }
+        }
+        
+    }
+}
--- Neekon/Neekon/MapViewController.swift
+++ Neekon/Neekon/MapViewController.swift
@@ -12,18 +12,19 @@ import Parse
 
 class MapViewController: UIViewController {
     
-    // Use a computed property for schedule to trigger updates when it's set after network calls
-    var scheduleItem:PFObject!
-    var schedule:PFObject! {
-        set {
-            scheduleItem = newValue as PFObject
-            updateAddress()
-            updateMap()
-        }
-        get {
-           return scheduleItem
-        }
-    }
+//    // Use a computed property for schedule to trigger updates when it's set after network calls
+//    var scheduleItem:PFObject!
+//    var schedule:PFObject! {
+//        set {
+//            scheduleItem = newValue as PFObject
+//            updateAddress()
+//            updateMap()
+//        }
+//        get {
+//           return scheduleItem
+//        }
+//    }
+    var eventInfoObject:EventInfoObject?
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var addressLabel: UILabel!
@@ -40,7 +41,12 @@ class MapViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.clearColor()
 
-        fetchSchedule()
+        EventInfoObject.fetchEventInfoObject { (eventInfoObject, error) -> Void in
+            if (error == nil) {
+                self.eventInfoObject = eventInfoObject;
+                self.updateAddress()
+            }
+        }
     }
 
     @IBAction func didTapGetDirection(sender: AnyObject) {
@@ -51,47 +57,33 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func fetchSchedule() {
-        let query = PFQuery(className: "Schedule")
-        query.findObjectsInBackgroundWithBlock { (results:[AnyObject]!, error:NSError?) -> Void in
-            if error != nil {
-                // TODO: Show Error to user
-                NSLog("Failed to load Schedule object from Parse")
-                NSLog(error!.localizedDescription)
-                return
-            }
-            
-            if let resultSchedule = results[0] as? PFObject {
-                self.schedule = resultSchedule
-            } else {
-                NSLog("wrong object was fetched")
-            }
+    
+    func updateAddress() {
+        if let address = eventInfoObject?.locationName {
+            addressLabel.text = "Address: \(address)"
+        } else {
+            addressLabel.text = ""
         }
     }
     
-    func updateAddress() {
-        let address = schedule.objectForKey("location") as NSString
-        addressLabel.text = "Address: \(address)"
-    }
-    
-    func updateMap() {
-        let address = schedule.objectForKey("location") as NSString
-        let latitude = schedule.objectForKey("latitude") as NSString
-        let longitude = schedule.objectForKey("longitude") as NSString
-        
-        let coordinates = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude.doubleValue) , longitude: CLLocationDegrees(longitude.doubleValue))
-        let region = MKCoordinateRegionMakeWithDistance(coordinates, 3000, 3000)
-        let adjustedRegion = mapView.regionThatFits(region)
-        
-        // Add pin
-        let point = MKPointAnnotation()
-        point.coordinate = coordinates
-        mapView.addAnnotation(point)
-
-        mapView.setRegion(region, animated: true)
-        mapView.showsUserLocation = true
-    }
+//    func updateMap() {
+//        
+//        let address = schedule.objectForKey("location") as NSString
+//        let latitude = schedule.objectForKey("latitude") as NSString
+//        let longitude = schedule.objectForKey("longitude") as NSString
+//        
+//        let coordinates = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude.doubleValue) , longitude: CLLocationDegrees(longitude.doubleValue))
+//        let region = MKCoordinateRegionMakeWithDistance(coordinates, 3000, 3000)
+//        let adjustedRegion = mapView.regionThatFits(region)
+//        
+//        // Add pin
+//        let point = MKPointAnnotation()
+//        point.coordinate = coordinates
+//        mapView.addAnnotation(point)
+//
+//        mapView.setRegion(region, animated: true)
+//        mapView.showsUserLocation = true
+//    }
 
 }
 
